41be5aff6816871cc1427bab8066771100390479
Daniil <boiko.81@list.ru>
2019-10-21 18:47:57 +0300

    you can add active tasks with default text

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var num = 1;
 
 function addQuestion() {
     var newdiv = document.createElement("div");
-    newdiv.innerHTML = "<div id='original-wrapper'>\n<div  class='form-1'>\n<form id='question' method='post'>\n<div><strong>Вопрос №[ " + num++ + "]<\/strong><\/div>\n<div style='float:left'><strong>Тип вопроса&nbsp;<\/strong><\/div>\n<div><select>\n<option>С вариантами ответа<\/option>\n<option>На сопоставление<\/option>\n<option>С полем ввода ответа<\/option>\n<\/select>\n<\/div>\n<div><strong>Описание вопроса<\/strong><\/div><\/form><\/div><\/div>";
+    newdiv.innerHTML = "<div id='active-task'><div id='task-title'>Название задачи</div><div id='task-desc'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</div></div>";
     //newdiv.appendTo('div#quest');
     document.getElementById("parentId").appendChild(newdiv);
     return false;

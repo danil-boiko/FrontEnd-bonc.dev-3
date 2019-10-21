577645fea08d001088268be0f2138b5eac531643
Daniil <boiko.81@list.ru>
2019-10-21 19:10:24 +0300

    added input data reading

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-var num = 1;
-
 function addQuestion() {
-    var newdiv = document.createElement("div");
-    newdiv.innerHTML = "<div id='active-task'><div id='task-title'>Название задачи</div><div id='task-desc'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</div></div>";
-    //newdiv.appendTo('div#quest');
+
+    var name = document.getElementById("task-name-input").value; //забираем название задачи
+    var desc = document.getElementById("task-desc-input").value; //забираем описание задачи
+    
+    //создаем новый блок и присваиваем переменной newdiv
+    var newdiv = document.createElement("div"); 
+    //заполняем созданный блок
+    newdiv.innerHTML = "<div id='active-task'><div id='task-title'>Задача без названия</div><div id='task-desc'>Описание отсутствует</div></div>";
     document.getElementById("parentId").appendChild(newdiv);
     return false;
 }

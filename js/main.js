770497b055a2e572c511feb037b578440543f4e7
Daniil <boiko.81@list.ru>
2019-10-21 20:47:24 +0300

    add bad function for edit task

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 function addTask() {
 
     var name = document.getElementById("task-name-input").value; //забираем название задачи
-    console.log(name);
     var desc = document.getElementById("task-desc-input").value; //забираем описание задачи
-    console.log(desc);
     //создаем новый блок и присваиваем переменной newdiv
     var newdiv = document.createElement("div"); 
     //заполняем созданный блок
-    newdiv.innerHTML = `<div class="task"><div id='active-task'><div id='task_title'>${name}</div><div id='task_desc'>${desc}</div></div><div class='task-buttons'><button class='edit-btn'>E</button><button class='delete-btn'>C</button></div></task>`;
+    newdiv.innerHTML = `<div class="task"><div id='active-task'><div id='task_title'>${name}</div><div id='task_desc'>${desc}</div></div><div class='task-buttons'><button class='edit-btn' data-toggle="modal" data-target="#editModal">E</button><button class='delete-btn'>C</button></div></task>`;
     document.getElementById("parentId").append(newdiv);
+}
+
+function editTask() {
+    var name = document.getElementById("task-name-input-edit").value; //забираем название задачи
+    var desc = document.getElementById("task-desc-input-edit").value; //забираем описание задачи
+    console.log(name);
+    console.log(desc);
+    task_title.innerHTML = '';
+    task_desc.innerHTML = '';
 }

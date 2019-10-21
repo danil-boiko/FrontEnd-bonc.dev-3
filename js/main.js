94f59c331a6c663356d725443cc59947f3a08ba2
Daniil <boiko.81@list.ru>
2019-10-21 20:17:46 +0300

    add: two buttons for delete and edit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-function addQuestion() {
+function addTask() {
 
     var name = document.getElementById("task-name-input").value; //забираем название задачи
+    console.log(name);
     var desc = document.getElementById("task-desc-input").value; //забираем описание задачи
-    
+    console.log(desc);
     //создаем новый блок и присваиваем переменной newdiv
     var newdiv = document.createElement("div"); 
     //заполняем созданный блок
-    newdiv.innerHTML = "<div id='active-task'><div id='task-title'>Задача без названия</div><div id='task-desc'>Описание отсутствует</div></div>";
-    document.getElementById("parentId").appendChild(newdiv);
-    return false;
+    newdiv.innerHTML = `<div class="task"><div id='active-task'><div id='task_title'>${name}</div><div id='task_desc'>${desc}</div></div><div class='task-buttons'><button class='edit-btn'>E</button><button class='delete-btn'>C</button></div></task>`;
+    document.getElementById("parentId").append(newdiv);
 }
